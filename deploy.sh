fe1fdfe4830207480b879b66b24d6f9a1b0bd54f
LinetTheFox <linet_moreandil@pm.me>
2022-11-12 00:47:17 +0200

    added placeholder for the deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+# Planning to write a script here that will perform automatic
+# deployment of the dotfiles along with getting the necessary
+# package dependencies - depending on the distro. Can't 
+# promise to cover all of them, but will surely try to make
+# it run at least on Arch Linux that I myself use :D
+
